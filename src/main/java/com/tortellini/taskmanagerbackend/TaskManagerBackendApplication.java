ce11c4647fc580868de726f63b21eb9b52a9ca4a
Hactually Benji <mrbenamiller@gmail.com>
2023-11-03 19:06:12 -0700

    Initial commit

---
+++ src/main/java/com/tortellini/taskmanagerbackend/TaskManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tortellini.taskmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tortellini/taskmanagerbackend/TaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tortellini.taskmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

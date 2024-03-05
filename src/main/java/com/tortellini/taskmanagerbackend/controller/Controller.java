efa2baa613b38710c0afb627c016ee9d206b2a56
HactuallyBenji <mrbenamiller@gmail.com>
2024-03-04 19:51:12 -0800

    Adding some state and second endpoint for breakpoint testing

--- src/main/java/com/tortellini/taskmanagerbackend/controller/Controller.java
+++ src/main/java/com/tortellini/taskmanagerbackend/controller/Controller.java
@@ -8,6 +8,13 @@ public class Controller {
 
     @GetMapping("/helloworld")
     public String helloWorld() {
+        int testVariable = 10;
+        System.out.println(testVariable);
         return "Hello World";
+    }
+
+    @GetMapping("/secondTest")
+    public String secondTest() {
+        return "Second Test";
     }
 }
